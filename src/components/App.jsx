ce948111156ce6f65752224d77e0fc516a9ff3dc
Vinay62000 <125733354+Vinay62000@users.noreply.github.com>
2024-02-09 23:25:41 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import RenderOnAnonymous from "./RenderOnAnonymous";
 import RenderOnAuthenticated from "./RenderOnAuthenticated";
 import Welcome from "./Welcome";
 
+
 const App = ({ store }) => (
   <Provider store={store}>
     <BrowserRouter>
